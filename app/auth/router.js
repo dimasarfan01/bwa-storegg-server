71582fef11af496089900d20976bb30127290d79
dimasarfan01 <dimasarfanasyuro123@gmail.com>
2021-09-16 16:36:35 +0700

    fix multer

--- app/auth/router.js
+++ app/auth/router.js
@@ -2,7 +2,9 @@ var express = require("express");
 var router = express.Router();
 const { signup, signin } = require("./controller");
 const multer = require("multer");
-const upload = multer();
+const upload = multer({
+  limits: { fieldSize: 25 * 1024 * 1024 },
+});
 const os = require("os");
 
 router.post("/signup", upload.none(), signup);
